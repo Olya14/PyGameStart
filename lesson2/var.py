802f0bf93b186d4320c06f482ed64c2f7d479280
Оля <olya@MacBook-Air-Ola.local>
2020-09-20 12:02:59 +0300

    first commit

---
+++ lesson2/var.py
@@ -0,0 +1,10 @@
+#float()
+#str()
+# this is my hints
+name = str(input('Enter your name: '))
+print('Hello', name)
+
+
+age = int(input('Enter your age: '))
+#age += 10 # age = age + 10
+print('Your age is', age+10)
---
+++ lesson3/functions.py
@@ -0,0 +1,11 @@
+def hello():
+    print('Hello')
+
+def hello2 (name, age):
+    year = 2020 - age
+    print('Hello', name, 'your year of birth is:', year)
+    print('Hello '+name, 'your year of birth is:', year)
+
+
+hello()
+hello2('Olya', 16)
